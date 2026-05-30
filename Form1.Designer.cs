3852468e9f3e94ac10fa41fd0d5ad3f548521d0d
woongah <105185651+woongah@users.noreply.github.com>
2026-05-30 22:01:40 +0900

    Add files via upload

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace WindowsFormsApp1
+﻿namespace Fanoperator
 {
     partial class Form1
     {
@@ -29,57 +29,58 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.btnApply = new System.Windows.Forms.Button();
-            this.tbxDirection = new System.Windows.Forms.TextBox();
-            this.tbxWeight = new System.Windows.Forms.NumericUpDown();
-            ((System.ComponentModel.ISupportInitialize)(this.tbxWeight)).BeginInit();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Location = new System.Drawing.Point(458, 96);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "btn";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // btnApply
+            // label1
             // 
-            this.btnApply.Location = new System.Drawing.Point(468, 98);
-            this.btnApply.Name = "btnApply";
-            this.btnApply.Size = new System.Drawing.Size(75, 23);
-            this.btnApply.TabIndex = 1;
-            this.btnApply.Text = "Apply";
-            this.btnApply.UseVisualStyleBackColor = true;
-            this.btnApply.Click += new System.EventHandler(this.btnApply_Click);
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(325, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
             // 
-            // tbxDirection
+            // label2
             // 
-            this.tbxDirection.Location = new System.Drawing.Point(118, 98);
-            this.tbxDirection.Name = "tbxDirection";
-            this.tbxDirection.Size = new System.Drawing.Size(100, 25);
-            this.tbxDirection.TabIndex = 2;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(325, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
             // 
-            // tbxWeight
+            // label3
             // 
-            this.tbxWeight.Location = new System.Drawing.Point(270, 99);
-            this.tbxWeight.Name = "tbxWeight";
-            this.tbxWeight.Size = new System.Drawing.Size(120, 25);
-            this.tbxWeight.TabIndex = 3;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(325, 200);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.tbxWeight);
-            this.Controls.Add(this.tbxDirection);
-            this.Controls.Add(this.btnApply);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.tbxWeight)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -88,9 +89,9 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button btnApply;
-        private System.Windows.Forms.TextBox tbxDirection;
-        private System.Windows.Forms.NumericUpDown tbxWeight;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
