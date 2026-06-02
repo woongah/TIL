daa06077c22e3a142d7e800c7c212be8cdcb79ff
woongah <105185651+woongah@users.noreply.github.com>
2026-06-02 23:48:53 +0900

    Add files via upload

---
+++ ArduinoControlApp1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace ArduinoControlApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.cmbPort = new System.Windows.Forms.ComboBox();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(583, 85);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(168, 23);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "btnConnect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(583, 127);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(168, 32);
+            this.btnDisconnect.TabIndex = 1;
+            this.btnDisconnect.Text = "btnDisconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // cmbPort
+            // 
+            this.cmbPort.FormattingEnabled = true;
+            this.cmbPort.Location = new System.Drawing.Point(444, 88);
+            this.cmbPort.Name = "cmbPort";
+            this.cmbPort.Size = new System.Drawing.Size(121, 23);
+            this.cmbPort.TabIndex = 2;
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(162, 88);
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(258, 25);
+            this.txtLog.TabIndex = 3;
+            this.txtLog.Text = "txtLog";
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(583, 183);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(168, 23);
+            this.btnRefresh.TabIndex = 4;
+            this.btnRefresh.Text = "btnRefresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.cmbPort);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.btnConnect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.ComboBox cmbPort;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.Button btnRefresh;
+    }
+}
+
---
+++ ArduinoControlApp1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArduinoControlApp
+{
+    public partial class Form1 : Form
+    {
+        //
+        private SerialPort serialPort;
+
+        public Form1()
+        {
+            InitializeComponent();
+            serialPort = new SerialPort();
+
+
+            // 아두이노가 데이터를 보냈을 때 자동으로 실행될 '이벤트 핸들러' 연결
+            serialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+        }
+
+        // 화면이 켜질 때 현재 PC에 연결된 COM 포트 목록을 자동으로 가져옵니다.
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+            cmbPort.Items.AddRange(ports);
+            if (cmbPort.Items.Count > 0) cmbPort.SelectedIndex = 0; // 첫 번째 포트 기본 선택
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (cmbPort.SelectedItem == null)
+            {
+                MessageBox.Show("연결할 COM 포트를 선택해 주세요!", "알림", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (!serialPort.IsOpen)
+            {
+                try
+                {
+                    serialPort.PortName = cmbPort.SelectedItem.ToString();
+                    serialPort.BaudRate = 9600;
+                    serialPort.Open();
+                    MessageBox.Show($"{serialPort.PortName} 포트에 성공적으로 연결되었습니다.");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"연결 실패: {ex.Message}");
+                }
+            }
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            if (serialPort.IsOpen)
+            {
+                serialPort.Close();
+                MessageBox.Show("연결이 해제되었습니다.");
+            }
+        }
+
+        private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            string inData = sp.ReadLine(); // 아두이노의 Serial.println() 데이터를 한 줄 읽어옴
+
+            // 크로스 스레드 에러 방지를 위한 Invoke 처리 (UI 스레드에 데이터 전달)
+            this.Invoke(new MethodInvoker(delegate {
+                txtLog.AppendText($"[{DateTime.Now:HH:mm:ss}] 수신: {inData}\r\n");
+            }));
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            cmbPort.Items.Clear(); // 기존 목록 지우기
+            string[] ports = SerialPort.GetPortNames(); // 현재 연결된 포트 새로 가져오기
+            cmbPort.Items.AddRange(ports);
+
+            if (cmbPort.Items.Count > 0) cmbPort.SelectedIndex = 0;
+            else MessageBox.Show("연결된 아두이노를 찾을 수 없습니다.");
+        }
+    }
+}
